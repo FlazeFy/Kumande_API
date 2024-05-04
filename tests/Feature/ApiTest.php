80293de45ab759cd426d84f63d5af8624b43f8e6
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2024-05-04 12:25:47 +0700

    [TEST] TS-S001, TS-S002, TS-S003, TS-S004, TS-S005, TS-S006

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -56,7 +56,91 @@ class ApiTest extends TestCase
     public function test_get_sign_out(): void
     {
         $token = $this->test_post_login();
-        $response = $this->httpClient->get("/api/v1/logout", [
+        $response = $this->httpClient->post("/api/v1/logout", [
+            'headers' => [
+                'Authorization' => "Bearer $token"
+            ]
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $data = json_decode($response->getBody(), true);
+        $this->assertArrayHasKey('message', $data);
+    }
+
+    // TC-S001
+    public function test_get_today_schedule(): void
+    {
+        $token = $this->test_post_login();
+        $response = $this->httpClient->get("/api/v1/schedule/day/Sat", [
+            'headers' => [
+                'Authorization' => "Bearer $token"
+            ]
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $data = json_decode($response->getBody(), true);
+        $this->assertArrayHasKey('message', $data);
+    }
+
+    // TC-S002
+    public function test_get_monthly_payment_analytic(): void
+    {
+        $token = $this->test_post_login();
+        $response = $this->httpClient->get("/api/v1/analytic/payment/month/5/year/2024", [
+            'headers' => [
+                'Authorization' => "Bearer $token"
+            ]
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $data = json_decode($response->getBody(), true);
+        $this->assertArrayHasKey('message', $data);
+    }
+
+    // TC-S003
+    public function test_get_today_calories(): void
+    {
+        $token = $this->test_post_login();
+        $response = $this->httpClient->get("/api/v1/count/calorie/fulfill/2024-05-04", [
+            'headers' => [
+                'Authorization' => "Bearer $token"
+            ]
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $data = json_decode($response->getBody(), true);
+        $this->assertArrayHasKey('message', $data);
+    }
+
+    // TC-S004
+    public function test_get_most_consume_type(): void
+    {
+        $token = $this->test_post_login();
+        $response = $this->httpClient->get("/api/v1/consume/total/bytype", [
+            'headers' => [
+                'Authorization' => "Bearer $token"
+            ]
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $data = json_decode($response->getBody(), true);
+        $this->assertArrayHasKey('message', $data);
+    }
+
+    // TC-S005
+    public function test_get_most_consume_from(): void
+    {
+        $token = $this->test_post_login();
+        $response = $this->httpClient->get("/api/v1/consume/total/byfrom", [
+            'headers' => [
+                'Authorization' => "Bearer $token"
+            ]
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $data = json_decode($response->getBody(), true);
+        $this->assertArrayHasKey('message', $data);
+    }
+
+    // TC-S006
+    public function test_get_most_consume_provide(): void
+    {
+        $token = $this->test_post_login();
+        $response = $this->httpClient->get("/api/v1/consume/total/byprovide", [
             'headers' => [
                 'Authorization' => "Bearer $token"
             ]
